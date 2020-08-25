5143c5d260fd67f7cdac3fd4e6462a45e6989dbe
Billzhuyf <50048868+Billzhuyf@users.noreply.github.com>
2020-08-25 20:41:49 +0800

    Clas__01 Upload

---
+++ Assets/script/AddForceLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceLogic : MonoBehaviour
+{
+    Rigidbody m_rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_rigidbody.AddForce(Vector3.up * 1000.0f);
+        }
+        
+    } 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("On Trigger Entered");
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/script/PlayerLogic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    float m_horizontallnput;
+    float m_verticallnput;
+
+    float m_movementSpeed = 5.0f;
+
+    CharacterController m_characterController;
+    Vector3 m_movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_horizontallnput = Input.GetAxis("Horizontal");
+        m_verticallnput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        m_movement.x = m_horizontallnput * m_movementSpeed * Time.deltaTime;
+        m_movement.z = m_verticallnput * m_movementSpeed * Time.deltaTime;
+        m_characterController.Move(m_movement);
+    }
+}
